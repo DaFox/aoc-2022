f3618dc7f53b4445825f600c3f1c3d9660bfeec3
DaFox <dafox@gmx.com>
2022-12-02 19:05:34 +0100

    solution for day 1 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -26,4 +26,28 @@ pub fn solve() {
         .sum();
 
     println!("Total score after following the strategy: {}", total);
+
+    let total:u32 = lines
+	.iter()
+	.map(|line| {
+             match line.split_once(" ") {
+                Some(("A", "X")) => 0 + 3, // Rock + Loss
+                Some(("A", "Y")) => 3 + 1, // Rock + Draw
+                Some(("A", "Z")) => 6 + 2, // Rock + Win
+
+                Some(("B", "X")) => 0 + 1, // Loss + Rock
+                Some(("B", "Y")) => 3 + 2, // Draw + Paper
+                Some(("B", "Z")) => 6 + 3, // Win  + Scissors
+
+                Some(("C", "X")) => 0 + 2, // Win  + Rock
+                Some(("C", "Y")) => 3 + 3, // Loss + Paper
+                Some(("C", "Z")) => 6 + 1, // Draw + Scissors
+
+                _ => 0	             // invalid
+             }
+        })
+        .into_iter()
+        .sum();
+
+    println!("{}", total);
 }
